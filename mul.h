69f5a315eae86234185a1cc7bb75770fb901fe7b
Jonathan <jlicari@ryerson.ca>
2019-03-02 18:16:52 +0000

    mul.h edited:
    Completed multiplier hardware definitions.

--- mul.h
+++ mul.h
@@ -7,7 +7,22 @@ void make_mul();
 
 SC_MODULE(mul) {
 
+// PORT DECLARATION
+	sc_in<bool> clk;
+	//sc_inout<sc_bv<4> > rA, rB;
+	sc_inout<sc_bv<8> > rA, rB;
+	//sc_inout<sc_bv<8> > sout; // 8 bit input registers and sum out
+	sc_inout<sc_bv<16> > sout;
+	
+// FUNCTION DECLARATION
+	void multiply(); // args?
 
+// CONSTRUCTOR
+	SC_CTOR(mul) {
 
+	SC_METHOD(multiply);
+	dont_initialize();
+	sensitive << clk.pos();
+	}
 };
 #endif
